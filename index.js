0a261157041d004ab111f4054685923f6136a0a5
claracmy <cheung.cmy@gmail.com>
2017-11-10 17:09:09 +0000

    hardcoded watson request in index.js

--- index.js
+++ index.js
@@ -9,9 +9,34 @@ const errorHandler    = require('./lib/errorHandler');
 const app             = express();
 const enviroment      = app.get('env');
 
+const TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
+const fs = require('fs');
+
 const mongoose        = require('mongoose');
 mongoose.Promise      = require('bluebird');
 mongoose.connect(db[enviroment], { useMongoClient: true });
+
+
+app.get('/', useWatson);
+
+function useWatson(req, res) {
+  console.log('watson running');
+  var text_to_speech = new TextToSpeechV1 ({
+    username: 'd643b333-42a6-4c0c-a04d-65d946d7b203',
+    password: 'nQfY6cXErZ8v'
+  });
+
+  var params = {
+    text: 'Hello clara',
+    voice: 'en-US_AllisonVoice',
+    accept: 'audio/wav'
+  };
+
+  // Pipe the synthesized text to a file.
+  text_to_speech.synthesize(params).on('error', function(error) {
+    console.log('Error:', error);
+  }).pipe(fs.createWriteStream('hello_clara.wav'));
+}
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
