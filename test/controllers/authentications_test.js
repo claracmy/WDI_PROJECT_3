42f3a86e24621e0b26a395d4ecb9a62396c6e3d3
Ma5ee2 <hussainmasee@gmail.com>
2017-11-11 14:03:28 +0000

    fixing registration tests

--- test/controllers/authentications_test.js
+++ test/controllers/authentications_test.js
@@ -17,16 +17,14 @@ describe('Authentications', function() {
         .post('/api/register')
         .set('Accept','application/json')
         .send({
-          user: {
-            username: 'billy',
+            username: 'Billy',
             email: 'a@a.com',
             passwordHash: 'password'
-          }
         })
         .end((err, res) => {
           expect(res.status).to.eq(200);
           expect(res.body).to.be.a('object');
-          expect(res.body.message).to.eq('Thanks for registering.');
+          expect(res.body.message).to.eq('Welcome Billy!');
           expect(res.body.token).to.be.a('string');
           done();
         });
@@ -36,15 +34,13 @@ describe('Authentications', function() {
         .post('/api/register')
         .set('Accept','application/json')
         .send({
-          user: {
-            username: 'billy',
+            username: 'Billy',
             passwordHash: 'password'
-          }
         })
         .end((err, res) => {
           expect(res.status).to.eq(400);
           expect(res.body).to.be.a('object');
-          expect(res.body.errors).to.eq('Validation Error: email: Path email is required');
+          expect(res.body.errors).to.eq('ValidationError: email: Path `email` is required.');
           expect(res.body.message).to.eq('Bad Request');
 
           done();
