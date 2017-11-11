602ff6312604103947b98ef94d8232aab2b50437
harryr89 <harry.robertson.harry@gmail.com>
2017-11-11 16:49:26 +0000

    file controller

---
+++ controllers/files.js
@@ -0,0 +1,50 @@
+const file = require('../models/file');
+
+function filesIndex (req, res){
+  File
+    .find()
+    .exec()
+    .then(files => res.status(200).json(files))
+    .catch(() => res.status(500).json({ message: 'something is wrong'}));
+
+}
+
+function filesShow (req, res){
+  File
+    .findById(req.params.id)
+    .exec()
+    .then(() => {
+      if (!file) return res.status(200).json({ message: 'file not found'});
+      return res.status(200).json(file);
+    })
+    .catch(() => res.status(500).json({ message: 'something went wrong'}));
+}
+
+function filesUpdate(req, res){
+  File
+    .findByIdAndUpdate(req.params.id)
+    .exec()
+    .then(() => {
+      if (!file) return res.status(404).json({ message: 'file not found '});
+      return res.status(200).json( { file } );
+    })
+    .catch(() => res.status(500).json({ message: 'something went wrong' }));
+}
+
+function filesDelete(req, res){
+  File
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => {
+      if (!file) return res.status(404).json({ message: 'file not found'});
+      return res.sendStatus(204);
+    })
+    .catch(() => res.status(500).json({ message: 'something went wrong' }));
+}
+
+module.exports = {
+  index: filesIndex,
+  show: filesShow,
+  update: filesUpdate,
+  delete: filesDelete
+};
