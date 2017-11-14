5d0fc46470d3638397d24de4cc92ebac723d3b90
claracmy <cheung.cmy@gmail.com>
2017-11-14 10:54:40 +0000

    add comments functioning

--- models/file.js
+++ models/file.js
@@ -7,11 +7,6 @@ const commentSchema = new mongoose.Schema({
   timestamps: true
 });
 
-commentSchema.methods.belongsTo = function commentBelongsTo(user) {
-  if(typeof this.createdBy.id === 'string') return this.createdBy.id === user.id;
-  return user.id === this.createdBy.toString();
-};
-
 const fileSchema = new mongoose.Schema({
   filename: { type: String, required: true },
   createdBy: { type: mongoose.Schema.ObjectId, ref: 'User' },
@@ -21,11 +16,5 @@ const fileSchema = new mongoose.Schema({
 }, {
   timestamps: true
 });
-
-fileSchema.methods.belongsTo = function fileBelongsTo(user) {
-  if(typeof this.createdBy.id === 'string') return this.createdBy.id === user.id;
-  return user.id === this.createdBy.toString();
-};
-
 
 module.exports = mongoose.model('File', fileSchema);
