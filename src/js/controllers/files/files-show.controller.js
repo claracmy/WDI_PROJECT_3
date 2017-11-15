8659bc3df2997527041324c04e7b3c91794397d3
claracmy <cheung.cmy@gmail.com>
2017-11-15 10:14:25 +0000

    like routes

--- src/js/controllers/files/files-show.controller.js
+++ src/js/controllers/files/files-show.controller.js
@@ -6,7 +6,6 @@ filesShowCtrl.$inject = ['File', '$stateParams', '$state'];
 
 function filesShowCtrl(File, $stateParams, $state) {
   const vm = this;
-  console.log('this is vm', vm);
   vm.boolean = true;
 
   File
@@ -14,34 +13,21 @@ function filesShowCtrl(File, $stateParams, $state) {
     .$promise
     .then((file) => {
       vm.file = file;
-      console.log('this is vm.file', vm.file);
     });
 
   vm.editTitle = editTitle;
   vm.showEditForm = showEditForm;
 
   vm.submitComment = function() {
-    console.log('submit comment function is fired');
     File
       .addComment({ id: vm.file._id }, vm.comment)
       .$promise
       .then(() => {
-        console.log('this is vm in the submit function', vm);
         vm.comment = {};
         vm.file = File.get({ id: $stateParams.id });
       });
   };
 
-<<<<<<< HEAD
-  // vm.deleteComment = function(comment) {
-  //   File
-  //     .deleteComment({id: vm.file._id, commentId: comment._id})
-  //     .$promise
-  //     .then(() => {
-  //       console.log('deleted');
-  //     });
-  // };
-=======
 
   vm.deleteComment = function(comment) {
     File
@@ -51,21 +37,17 @@ function filesShowCtrl(File, $stateParams, $state) {
         console.log('deleted');
       });
   };
->>>>>>> 377cdde49f955474471dde6bae25ff34170af1f9
+
 
   function showEditForm() {
-    console.log('boolean before edit', vm.boolean);
     vm.boolean = !vm.boolean;
-    console.log('boolean before edit', vm.boolean);
   }
 
   function editTitle() {
-    console.log('edittitle function fired');
     File
       .update({ id: $stateParams.id }, vm.file)
       .$promise
       .then(() => {
-        console.log('this is the new title', vm.file);
         vm.file = File.get({ id: $stateParams.id });
         vm.boolean = !vm.boolean;
       });
