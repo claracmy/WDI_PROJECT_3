089ff932bab6588219c8d990c189dc22af6ae7e9
harryr89 <harry.robertson.harry@gmail.com>
2017-11-16 19:28:48 +0000

    styled login and register

--- src/js/controllers/users/users-show.controller.js
+++ src/js/controllers/users/users-show.controller.js
@@ -1,51 +1,28 @@
-angular
-  .module('whatsOn')
-  .controller('usersShowCtrl', usersShowCtrl);
+angular.module('whatsOn').controller('usersShowCtrl', usersShowCtrl);
 
-usersShowCtrl.$inject = [
-  'User',
-  'File',
-  '$stateParams'
-];
-function usersShowCtrl(
-  User,
-  File,
-  $stateParams
-) {
+usersShowCtrl.$inject = ['User', 'File', '$stateParams'];
+function usersShowCtrl(User, File, $stateParams) {
   const vm = this;
-<<<<<<< HEAD
 
-=======
   // vm.deleteFile = deleteFile;
   console.log(vm);
->>>>>>> 314f6e33000f025ede70f5872ff2f492f4f6bb9d
-  User
-    .getCreatedFiles({ id: $stateParams.id })
-    .$promise
-    .then((files) => {
-<<<<<<< HEAD
-=======
-    // console.log('files passed to vm.files', files);
->>>>>>> 314f6e33000f025ede70f5872ff2f492f4f6bb9d
-      vm.files = files;
-    });
 
-  User
-    .get({ id: $stateParams.id })
-    .$promise
-    .then( user => {
-      vm.user = user;
+  User.getCreatedFiles({ id: $stateParams.id }).$promise.then(files => {
+    // console.log('files passed to vm.files', files);
+
+    vm.files = files;
+  });
+
+  User.get({ id: $stateParams.id }).$promise.then(user => {
+    vm.user = user;
     // console.log('user', user);
-    });
-<<<<<<< HEAD
-=======
+  });
 
   vm.play = url => {
     const audio = new Audio(url);
     audio.play();
     vm.isPlaying = true;
   };
-
 
   // function deleteFile(file) {
   //   console.log('delete button clicked');
@@ -57,6 +34,4 @@ function usersShowCtrl(
   // //       console.log('deleted');
   // //     });
   // }
-
->>>>>>> 314f6e33000f025ede70f5872ff2f492f4f6bb9d
 }
