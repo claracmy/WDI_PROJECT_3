5f40d01b776cd58051348090dbe2d4b583dd96a6
claracmy <cheung.cmy@gmail.com>
2017-12-31 00:00:25 +0000

    bower moved from dev dependancy to dependancy

--- src/js/controllers/users/users-show.controller.js
+++ src/js/controllers/users/users-show.controller.js
@@ -5,29 +5,18 @@ function usersShowCtrl(User, File, $stateParams) {
   const vm = this;
   vm.editProfile = editProfile;
 
-  User
-    .getCreatedFiles({ id: $stateParams.id })
-    .$promise
-    .then((files) => {
-      vm.files = files;
-    });
+  User.getCreatedFiles({ id: $stateParams.id }).$promise.then(files => {
+    vm.files = files;
+  });
 
-  User
-    .get({ id: $stateParams.id })
-    .$promise
-    .then( user => {
-      vm.user = user;
-    });
+  User.get({ id: $stateParams.id }).$promise.then(user => {
+    vm.user = user;
+  });
 
   function editProfile() {
-    console.log(vm.user.id);
-    console.log(vm.user);
-    User
-      .update({ id: vm.user.id }, vm.user)
-      .$promise
-      .then(() => {
-        vm.user = User.get({ id: vm.user.id });
-      });
+    User.update({ id: vm.user.id }, vm.user).$promise.then(() => {
+      vm.user = User.get({ id: vm.user.id });
+    });
   }
 
   const audio = new Audio();
